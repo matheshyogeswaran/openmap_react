d621a147ed8c17acf0573432826f292ee50ee18f
mathesh <matheshyogeswaran@outlook.com>
2023-01-18 14:05:29 +0530

    OSM intergrate done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
 import './App.css';
 import PropertiesMap from './pages/PropertiesMap';
 
 function App() {
   return (
     <div className="App">
-      <PropertiesMap/>
+       <PropertiesMap lan={7.291418} lon={80.636696}/>
     </div>
   );
 }
--- src/pages/PropertiesMap.js
+++ src/pages/PropertiesMap.js
@@ -1,7 +1,52 @@
+import { useState, useEffect } from "react";
+import L from 'leaflet/dist/leaflet';
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
-function PropertiesMap(props){
-    return(
-        <div>hi</div>
-    )
-}
+function PropertiesMap(props) {
+
+    let DefaultIcon = L.icon({ iconUrl: icon, shadowUrl: iconShadow, });
+
+    const[latitude,setLatitude]=useState("");
+    const[longitude,setLongitude]=useState("");
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(function(p){
+            setLatitude(p.coords.latitude);
+            setLongitude(p.coords.longitude);
+        },function(err){
+            console.warn("Error Code"+err.code+": "+err.message);
+            alert("Check the console");
+        });
+
+        var container = L.DomUtil.get("myMap");
+        if (container != null) { container._leaflet_id = null; } 
+
+        var map = L.map("myMap").setView([latitude,longitude],12);
+
+        L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+        {
+            attribution:'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:"pk.eyJ1IjoidGFyLWhlbCIsImEiOiJjbDJnYWRieGMwMTlrM2luenIzMzZwbGJ2In0.RQRMAJqClc4qoNwROT8Umg",
+        }
+        ).addTo(map);
+
+        L.Marker.prototype.options.icon = DefaultIcon;
+        var marker = L.marker([props.lan,props.lon]).addTo(map);
+        marker.bindPopup("<b>destination</b>").openPopup();
+        
+       // L.Marker.prototype.options.icon = DefaultIcon;
+        var marker1 = L.marker([latitude,longitude]).addTo(map);
+        marker1.bindPopup("<b>Your location</b>").openPopup();
+
+
+
+    });
+    return (<><div id="myMap" style={{ height: "100vh" }}></div></>);
+
+  }
+
 export default PropertiesMap;
